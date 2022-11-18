6b4f92ca84b140fec5081336c462555413887b58
ananos548 <inabsm@mail.ru>
2022-11-18 22:18:35 +0300

    design product category/details

--- store_project/urls.py
+++ store_project/urls.py
@@ -3,10 +3,10 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('store.urls'))
+    path('', include('store.urls')),
+    path('account', include('account.urls'))
 ]
 
 if settings.DEBUG:
